d40274715923484500180306895a03fac2d44a75
Tommy P. Keane <talk.to.tommypkeane@gmail.com>
2022-10-30 18:37:34 -0400

    [TST] Updated Unit-Testing

--- tests_unit/conftest.py
+++ tests_unit/conftest.py
@@ -7,3 +7,27 @@ Common fixtures and definitions for tests should go here, as this file will be
 automatically imported and parsed by `pytest`.
 """
 # pylint: disable=missing-function-docstring
+import json
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def plotly_example_sankey_energy_json() -> dict:
+    """Read JSON Data File into a pytest Fixture for Sankey Diagrams
+
+    Source for data captured on 2022-10-30 around 6pm ET with command:
+
+    ```bash
+    curl -X GET https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json -o ./_data/plotly_sankey_energy.json
+    ```
+
+    Returns:
+        dict: plotly Example `sankey_energy.json` Data read-in as a JSON Object
+    """
+    data = None
+    data_path = pathlib.Path("_data/plotly_sankey_energy.json")
+    with open(data_path, "r") as json_file:
+        data = json.load(json_file)
+    return data
--- tests_unit/plotly_dash_demos/test_alluvial_flow_sankey.py
+++ tests_unit/plotly_dash_demos/test_alluvial_flow_sankey.py
@@ -3,29 +3,54 @@ import urllib
 
 import pandas
 
-from plotly_dash_demos.alluvial_flow_sankey import sankey_diagram_from_dataframes
+from plotly_dash_demos.alluvial_flow_sankey import (
+    describe_sankey_diagram_from_dataframes,
+    sankey_diagram_from_dataframes,
+)
 
-def test_sankey_diagram_from_dataframes():
-    url = "https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json"
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
+
+def test_sankey_diagram_from_dataframes(plotly_example_sankey_energy_json):
     nodes_df = pandas.DataFrame(
         {
-            "label": data["data"][0]["node"]["label"],
-            "color": data["data"][0]["node"]["color"],
+            "label": plotly_example_sankey_energy_json["data"][0]["node"]["label"],
+            "color": plotly_example_sankey_energy_json["data"][0]["node"]["color"],
         },
     )
     links_df = pandas.DataFrame(
         {
-            "source": data["data"][0]["link"]["source"],
-            "target": data["data"][0]["link"]["target"],
-            "value": data["data"][0]["link"]["value"],
-            "color": data["data"][0]["link"]["color"],
+            "source": plotly_example_sankey_energy_json["data"][0]["link"]["source"],
+            "target": plotly_example_sankey_energy_json["data"][0]["link"]["target"],
+            "value": plotly_example_sankey_energy_json["data"][0]["link"]["value"],
+            "color": plotly_example_sankey_energy_json["data"][0]["link"]["color"],
         },
     )
     fig_obj = sankey_diagram_from_dataframes(
         nodes_df=nodes_df,
         links_df=links_df,
     )
-    fig_obj.show()
+    fig_obj.write_html("_plots/test_sankey_diagram_from_dataframes.html")
+    return None
+
+
+def test_describe_sankey_diagram_from_dataframes(plotly_example_sankey_energy_json):
+    nodes_df = pandas.DataFrame(
+        {
+            "label": plotly_example_sankey_energy_json["data"][0]["node"]["label"],
+            "color": plotly_example_sankey_energy_json["data"][0]["node"]["color"],
+        },
+    )
+    links_df = pandas.DataFrame(
+        {
+            "source": plotly_example_sankey_energy_json["data"][0]["link"]["source"],
+            "target": plotly_example_sankey_energy_json["data"][0]["link"]["target"],
+            "value": plotly_example_sankey_energy_json["data"][0]["link"]["value"],
+            "color": plotly_example_sankey_energy_json["data"][0]["link"]["color"],
+        },
+    )
+    info = describe_sankey_diagram_from_dataframes(
+        nodes_df=nodes_df,
+        links_df=links_df,
+    )
+    with open("_plots/test_describe_sankey_diagram_from_dataframes.json", "w+") as file_obj:
+        json.dump(info, file_obj, indent=2)
     return None
